ebff5aa11f9155eb5f2d9f98fd1686644796ffc4
Priya <84715586+Karpagapriyad@users.noreply.github.com>
2024-05-14 19:21:51 +0200

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -12,16 +12,16 @@ if uploaded_file is not None:
     st.image(uploaded_file, caption="Uploaded Image", use_column_width=True)
 
     if st.button("Segment Image"):
-        # Upload the image to FastAPI
+        # Uploading the image to FastAPI
         files = {'file': uploaded_file.getvalue()}
         response = requests.post('http://localhost:8000/segment-image', files=files)
 
         if response.status_code == 200:
-            # Get the segmented image
+            # Gettting the segmented image
             segmented_image_bytes = response.content
             segmented_image = Image.open(BytesIO(segmented_image_bytes))
 
-            # Display segmented image
+            # Displaying segmented image
             st.image(segmented_image, caption="Segmented Image", use_column_width=True)
         else:
             st.error("Error occurred while segmenting image.")
